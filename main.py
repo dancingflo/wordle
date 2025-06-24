9cc7e79b9689386c0bffa2d563af5a84ce08a78f
dancingflo <>
2025-06-24 21:22:49 +0100

    coloured marking and caught invalid

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from wordle import WordleGame
+from wordle import WordleGame, WordleGuessError
 
+COLORS = ["\x1b[39m", "\x1b[33m", "\x1b[32m"]
 
 def main():
     print("Welcome to Wordle!")
     game = WordleGame()
     while game.playing:
         guess = input()
-        mark = game.guess(guess)
-        print(mark)
-
+        try:
+            mark = game.guess(guess)
+            print("\x1b[A\r"+ "".join(COLORS[m]+l for m, l in zip(mark, guess)) +"\x1b[0m")
+        except WordleGuessError:
+            print("\x1b[A\r      (invalid)\r\x1b[A")
+        
 main()
