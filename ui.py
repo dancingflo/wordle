1d4e9e6536a5fffb36ce9d3242c75d0aaa6976be
dancingflo <>
2025-06-29 13:41:25 +0100

    added finish and min terminal size

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 from wordle import WordleGame, WordleGuessError
 from msvcrt import getch
@@ -6,11 +7,16 @@ from string import ascii_letters
 class WordleUI:
     LETTERS = set(bytes(c, "ascii") for c in ascii_letters)
     COLORS = ["\x1b[39m", "\x1b[33m", "\x1b[32m"]
+    MESSAGES = ["Magic!", "Genius!", "Amazing!", "Great!", "Solid!", "Phew!"]
     def __init__(self) -> None:
         self.game = WordleGame()
         self.guesses = []
     
     def run(self) -> None:
+        size = shutil.get_terminal_size()
+        if size.lines < self.game.max_guesses + 6 or size.columns < 11:
+            print("Terminal smaller than (12, 11)")
+            return
         self.message = ""
         self.buffer = b""
         self.refresh()
@@ -18,6 +24,7 @@ class WordleUI:
             char = getch()
             # self.message = str(char)
             if char == b"\x03":
+                print("\x1b[11B", end="")
                 sys.exit(1)
             if char == b"\x08":
                 if self.buffer:
@@ -30,6 +37,7 @@ class WordleUI:
                     if self.message:
                         self.message = ""
             self.refresh()
+        self.exit()
     
     def guess(self) -> None:
         try:
@@ -40,25 +48,34 @@ class WordleUI:
         except WordleGuessError:
             self.message = "invalid"
         self.buffer = b""
-    
+        
+    def exit(self) -> None:
+        if self.game.completed:
+            self.message = self.MESSAGES[self.game.guesses - 1]
+        else:
+            self.message = self.COLORS[2] + self.game.answer.center(11) + "\x1b[0m"
+        self.refresh()
+        print("\x1b[11B", end="")
+        
+        
     def refresh(self) -> None:
         lines = []
         for i in range(self.game.max_guesses):
             if i < len(self.guesses):
                 word = self.guesses[i]
-            elif i == len(self.guesses):
-                word = str(self.buffer, "ascii") + "_" * (5 - len(self.buffer)) # CAREFUL!
+            elif i == len(self.guesses) and self.game.playing:
+                word = "\x1b[1m" + str(self.buffer, "ascii") + "_" * (5 - len(self.buffer)) + "\x1b[22m" # CAREFUL!
             else:
-                word = "_____"
-            lines.append(f"| {i+1} {word}   |")
+                word = "_____" if self.game.playing else "     "
+            lines.append(f"| {i+1 if self.game.playing else ' '} {word}   |")
         # print("\r" + str(self.buffer, "ascii"), end="")
         print(f"""\
 +===========+
-|  Wordle!  |
-|  ({self.game.answer})   |
+|  \x1b[1mWordle!\x1b[22m  |
+|           |
 {'\n'.join(lines)}
 |           |
-|{self.message.center(11)}|
+|{self.message.center(11) if self.game.playing else self.message}|
 +===========+\x1b[{6+len(lines)}A""")
         print("\x1b[?25l", end="")
 
