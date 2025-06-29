f66cf979461cecdbd79cf9c2b785897c1850d001
dancingflo <>
2025-06-29 13:00:36 +0100

    added guessing

--- ui.py
+++ ui.py
@@ -1,27 +1,45 @@
 import sys
-from wordle import WordleGame
+from wordle import WordleGame, WordleGuessError
 from msvcrt import getch
 from string import ascii_letters
 
 class WordleUI:
     LETTERS = set(bytes(c, "ascii") for c in ascii_letters)
+    COLORS = ["\x1b[39m", "\x1b[33m", "\x1b[32m"]
     def __init__(self) -> None:
         self.game = WordleGame()
         self.guesses = []
     
     def run(self) -> None:
+        self.message = ""
         self.buffer = b""
         self.refresh()
         while self.game.playing:
             char = getch()
+            # self.message = str(char)
             if char == b"\x03":
                 sys.exit(1)
             if char == b"\x08":
                 if self.buffer:
                     self.buffer = self.buffer[:-1]
+            elif char == b"\r":
+                self.guess()
             elif char in self.LETTERS:
-                self.buffer += char
+                if len(self.buffer) < 5:
+                    self.buffer += char
+                    if self.message:
+                        self.message = ""
             self.refresh()
+    
+    def guess(self) -> None:
+        try:
+            guess = str(self.buffer, "ascii")
+            mark = self.game.guess(guess)
+            colourised = "".join(self.COLORS[m] + l for m, l in zip(mark, guess)) + "\x1b[0m"
+            self.guesses.append(colourised)
+        except WordleGuessError:
+            self.message = "invalid"
+        self.buffer = b""
     
     def refresh(self) -> None:
         lines = []
@@ -37,11 +55,12 @@ class WordleUI:
         print(f"""\
 +===========+
 |  Wordle!  |
-|           |
+|  ({self.game.answer})   |
 {'\n'.join(lines)}
 |           |
-| Message   |
-+===========+""")
+|{self.message.center(11)}|
++===========+\x1b[{6+len(lines)}A""")
+        print("\x1b[?25l", end="")
 
 def main():
     WordleUI().run()
